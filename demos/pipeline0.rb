42956ab39a752e84bdd4ef1db4b1a20047410b0f
Adib Saad <adib.saad@gmail.com>
2021-11-28 15:29:23 -0500

    Update pipeline example

--- demos/pipeline0.rb
+++ demos/pipeline0.rb
@@ -1,56 +1,54 @@
 require 'nng'
-require 'set'
 
-ADDR = "ipc:///tmp/pipeline.ipc"
-
-def start_pusher(i)
-  sock = NNG::Socket::Push0.new
-  sock.dial(ADDR)
-  loop_times = (rand * 10).ceil
-  puts "[pusher #{i}] sending #{loop_times} messages"
-  loop_times.times do |l_i|
-    sock.send_msg("msg-#{l_i} from pusher-#{i}")
-    sleep 0.1
-  end
-  puts "[pusher #{i}] finished"
-  sock.send_msg("fin-#{i}")
-end
-
-def start_puller(total_pushers)
+def start_puller(i)
   sock = NNG::Socket::Pull0.new
-  sock.listen(ADDR)
-  running_set = Set.new
-  total_pushers.times do |i|
-    running_set.add i
-  end
-  puts "[puller] listening at #{ADDR}"
+  addr = "ipc:///tmp/puller-#{i}.ipc"
+  sock.listen(addr)
+  puts "[puller] listening at #{addr}"
+
   loop do
     msg = sock.get_msg
     body = msg.body
-    if body.start_with? 'fin'
-      id = body.split('-')[1].to_i
-      running_set.delete(id)
-      puts "[puller] client #{id} sent 'fin' message (remaining: #{running_set.to_a.join(', ')})"
-      break if running_set.empty?
-    else
-      puts "[puller] got message: \"#{body}\""
-    end
+    break if body == "fin"
+    puts "[puller #{i}] got message: \"#{body}\""
   end
-  puts "[puller] finished"
+  puts "[puller #{i}] finished"
+end
+
+def start_pusher(total_pullers)
+  sock = NNG::Socket::Push0.new
+  total_pullers.times do |i|
+    sock.dial("ipc:///tmp/puller-#{i}.ipc")
+  end
+
+  loop_times = (rand * 10).ceil
+  puts "[pusher] sending #{loop_times} messages"
+  loop_times.times do |i|
+    sock.send_msg("msg-#{i}")
+    sleep 0.1
+  end
+
+  # Send fin to all pullers
+  total_pullers.times do
+    sock.send_msg("fin")
+  end
+
+  puts "[pusher] finished"
 end
 
 def start
   threads = []
-  total_pushers = 5
+  total_pullers = 5
 
-  threads << Thread.new { start_puller(total_pushers) }
-
-  # Wait for puller to start
-  sleep 0.1
-
-  total_pushers.times { |i|
-    threads << Thread.new { start_pusher i }
+  total_pullers.times { |i|
+    threads << Thread.new { start_puller i }
   }
+
+  # Wait for pullers to start
+  sleep 0.5
+
+  threads << Thread.new { start_pusher(total_pullers) }
+
   threads.each(&:join)
 end
 
