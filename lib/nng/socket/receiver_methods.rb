eee586af30ae2bb20f54495817bad7f9b790a6e4
Patrik Wenger <patrik.wenger@mindclue.ch>
2024-07-15 23:54:27 +0200

    add transparent support for Async
    This uses IO#wait_readable to wait for when a socket is
    readable/writable. That means it also won't block and ignore interrupts.

--- lib/nng/socket/receiver_methods.rb
+++ lib/nng/socket/receiver_methods.rb
@@ -1,6 +1,17 @@
+begin
+  require 'io/wait'
+rescue LoadError
+end
+
 module NNG
   module Socket
     module ReceiverMethods
+      def self.included(mod)
+        if ::IO.method_defined? :wait_readable
+          mod.prepend Async
+        end
+      end
+
       def recv_fd
         get_opt_int 'recv-fd'
       end
@@ -11,6 +22,26 @@ module NNG
 
       def recv_timeout=(seconds)
         set_opt_ms 'recv-timeout', seconds
+      end
+
+
+      module Async
+        def get_msg
+          super if wait_readable
+        end
+
+        def recv_io
+          @recv_io ||= ::IO.for_fd recv_fd, autoclose: false
+        end
+
+        def recv_timeout=(seconds)
+          super
+          recv_io.timeout = seconds
+        end
+
+        def wait_readable
+          recv_io.wait_readable
+        end
       end
     end
   end
