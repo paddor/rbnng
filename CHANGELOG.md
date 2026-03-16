fde74ccca2793e2affaf873cd0d7e6a1c9cee4e2
Patrik Wenger <paddor@gmail.com>
2026-03-16 21:09:18 +0100

    v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.0.1
+
+- Fix release workflow
+
 ## 1.0.0
 
 ### Breaking changes
--- lib/nng/version.rb
+++ lib/nng/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module NNG
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 end
